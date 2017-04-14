9f54e605548df0058ba4862f32e79659a8a7b12b
Lennart Frantzell <lennart_frantzell@yahoo.com>
2017-04-14 10:50:38 -0700

    Add files via upload

---
+++ Tone Analyzer/credentials.py
@@ -0,0 +1,9 @@
+#
+# Tone Analyzer Credentials
+#
+from watson_developer_cloud import ToneAnalyzerV3
+
+tone_analyzer = ToneAnalyzerV3(
+    username = '....................................',
+    password = '............',
+    version='2016-05-19 ')
